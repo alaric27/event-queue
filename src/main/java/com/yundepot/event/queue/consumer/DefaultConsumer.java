75c1f6a276ef11ab15cf0fb53739f6544512e44c
zhaiyanan <429276924@qq.com>
2024-06-14 19:15:52 +0800

    init

--- src/main/java/com/yundepot/event/queue/consumer/DefaultConsumer.java
+++ src/main/java/com/yundepot/event/queue/consumer/DefaultConsumer.java
@@ -2,6 +2,9 @@ package com.yundepot.event.queue.consumer;
 
 import com.yundepot.event.queue.Sequence;
 
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 /**
  * @author zhaiyanan
@@ -9,6 +12,13 @@ import com.yundepot.event.queue.Sequence;
  */
 public class DefaultConsumer implements Consumer {
     private final Sequence sequence = new Sequence(Sequence.INITIAL_VALUE);
+    private ThreadFactory threadFactory;
+
+    private static final int IDLE = 0;
+    private static final int HALTED = IDLE + 1;
+    private static final int RUNNING = HALTED + 1;
+
+    private final AtomicInteger running = new AtomicInteger(IDLE);
 
     @Override
     public Sequence getSequence() {
@@ -17,10 +27,29 @@ public class DefaultConsumer implements Consumer {
 
     @Override
     public void start() {
-
+        threadFactory.newThread(() -> run()).start();
     }
 
     private void run() {
-
+        int witnessValue = running.compareAndExchange(IDLE, RUNNING);
+        if (witnessValue == IDLE) {
+            sequenceBarrier.clearAlert();
+            notifyStart();
+            try {
+                if (running.get() == RUNNING) {
+                    processEvents();
+                }
+            } finally {
+                notifyShutdown();
+                running.set(IDLE);
+            }
+        }
+        else {
+            if (witnessValue == RUNNING) {
+                throw new IllegalStateException("Thread is already running");
+            } else {
+                earlyExit();
+            }
+        }
     }
 }
