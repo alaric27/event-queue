116569d8896e0c1ed352723adfbf86ff6eff1c37
zhaiyanan <429276924@qq.com>
2024-06-17 19:24:28 +0800

    init

--- src/main/java/com/yundepot/event/queue/consumer/WaitStrategy.java
+++ src/main/java/com/yundepot/event/queue/consumer/WaitStrategy.java
@@ -9,8 +9,14 @@ import com.yundepot.event.queue.common.Sequence;
  * @date 2024/6/14  10:24
  */
 public interface WaitStrategy {
+
     /**
+     *
+     * @param sequence 期待的序列起点
+     * @param cursorSequence broker的序列
+     * @param dependentSequence 依赖的序列
      * @return 最大可见的序列, 不一定可消费
+     * @throws Exception
      */
     long waitFor(long sequence, Sequence cursorSequence, Sequence dependentSequence) throws Exception;
 
