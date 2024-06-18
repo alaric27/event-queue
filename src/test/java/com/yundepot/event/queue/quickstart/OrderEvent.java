a6fa4d79d58b2d2bf93c70df7f2df6ff1f79f965
zhaiyanan <429276924@qq.com>
2024-06-18 16:24:55 +0800

    update

--- src/test/java/com/yundepot/event/queue/quickstart/OrderEvent.java
+++ src/test/java/com/yundepot/event/queue/quickstart/OrderEvent.java
@@ -1,12 +1,17 @@
 package com.yundepot.event.queue.quickstart;
 
-import lombok.Data;
-
 /**
  * @author zhaiyanan
  * @date 2024/6/17  16:03
  */
-@Data
 public class OrderEvent {
     private Long value;
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
 }
