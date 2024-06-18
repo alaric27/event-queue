ef24334666065287e433f864d0abe4f65636689a
zhaiyanan <429276924@qq.com>
2024-06-18 16:25:32 +0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@
  * @date 2024/6/12  15:34
  */
 module com.yundopot.event.queue {
-    requires static lombok;
-    requires org.slf4j;
     exports com.yundepot.event.queue;
     exports com.yundepot.event.queue.producer;
     exports com.yundepot.event.queue.consumer;
