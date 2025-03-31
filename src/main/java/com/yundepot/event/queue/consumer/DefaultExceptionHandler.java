5822f06895cf4f047c27d03ffe0b28383dc318ab
zhaiyanan <429276924@qq.com>
2025-03-31 10:50:36 +0800

    update

--- src/main/java/com/yundepot/event/queue/consumer/DefaultExceptionHandler.java
+++ src/main/java/com/yundepot/event/queue/consumer/DefaultExceptionHandler.java
@@ -12,7 +12,6 @@ public class DefaultExceptionHandler<T> implements ExceptionHandler<T> {
     @Override
     public void handleEventException(final Throwable ex, final long sequence, final Object event) {
         log.info("Exception processing: " + sequence + " " + event, ex);
-        throw new RuntimeException(ex);
     }
 
     @Override
